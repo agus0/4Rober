4e7f9ff94b0c11a5e99a901a03153a7197ee6177
Agustin Padron <agustinpadron94@gmail.com>
2017-12-21 09:34:07 -0300

    corrección de errores

--- 4Rober/4Rober.ino
+++ 4Rober/4Rober.ino
@@ -2,20 +2,32 @@
   Proyecto 4Rober ver Documentacion.
   Autor: Padron Agustin.
 */
+
+#include <TimedAction.h>    //Libreria para ejecutar procedimientos cada determinado tiempo
 #include <SoftwareSerial.h>   // Incluimos la librería  SoftwareSerial  
-SoftwareSerial BT(4,2);    // Definimos los pines RX y TX del Bluetooth
+
 
 //Variable de control de motores
 int izq  = 5;   //motor izquierdo borne para avanzar
 int izq2 = 6;   //motor izquierdo borne para retroceder
 int der  = 10;  //motor derecho borne para avanzar
 int der2 = 11;  //motor derecho borne para retroceder
+
 //Variables de sensores de distancia
 int echoPin_der = 7;
 int trigPin_der = 8;
 int echoPin_izq = 9;
 int trigPin_izq = 12;
 
+//variables de programa
+int     estado = 0;
+boolean protec_on = true;
+boolean sacudir_on = false;
+
+void sacudir();
+
+SoftwareSerial BT(4,2);    // Definimos los pines RX y TX del Bluetooth
+TimedAction sacudirT = TimedAction(250,sacudir);
 
 void setup() {
   //se seleccionan los pines de salida para motores
@@ -23,48 +35,48 @@ void setup() {
   pinMode(izq2, OUTPUT);
   pinMode(der, OUTPUT);
   pinMode(der2, OUTPUT);
-
-  BT.begin(9600);       // Inicializamos el puerto serie BT
   
-  avanzar(255);
-  delay(2000);
-  frenar(255);
-  delay(2000);
-  izquierda(255);
-  delay(2000);
-  derecha(255);
-  delay(2000);
-  retroceder(255);
-  delay(2000);
-  neutro();
+  BT.begin(9600);       // Inicializamos el puerto serie BT
 }
 
 void loop() {
+  if (sacudir_on) {
+    sacudirT.check();
+  }
+  EscuchaBluetooth();
+  if (protec_on){
+    proteccion();
+  }
+}
+
+//funcion de manejo de Bluetooth
+void EscuchaBluetooth() {
   //Si envian algo por BR
   if(BT.available()) {
     int comando;
     comando = BT.read();
+    sacudir_on = false;
     switch(comando) {
-      case 0:
+      case '0':
         neutro();
         break;
-      case 1:
+      case '1':
         avanzar(255);
         break;
-      case 2:
+      case '2':
         retroceder(255);
         break;
-      case 3:
+      case '3':
         izquierda(255);
         break;
-      case 4:
+      case '4':
         derecha(255);
         break;
-      case 5:
+      case '5':
         frenar(255);
         break;
-      case 6:
-        sacudir(1000, 10);
+      case '6':
+        sacudir_on = true;
         break;
     }
   }
@@ -75,8 +87,8 @@ void loop() {
 int medir(int trigPin,int echoPin){
   long durac;
   int dist;
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -88,13 +100,40 @@ int medir(int trigPin,int echoPin){
 }
 
 int medir_derecha() {
-  return (medir(trigPin_der, echoPin_der));
+  int medida = medir(trigPin_der, echoPin_der);
+  return medida;
 }
 
-int medir_izquiera() {
-  return (medir(trigPin_izq, echoPin_izq));
+int medir_izquierda() {
+  int medida = medir(trigPin_izq, echoPin_izq);
+  return medida;
 }
 
+boolean limite_derecha() {
+  int limite = 15;
+  boolean resultado = false;
+  if (medir_derecha() < limite) {
+    resultado = true;
+  }
+  return resultado;
+}
+
+boolean limite_izquierda() {
+  int limite = 15;
+  boolean resultado = false;
+  if (medir_izquierda() < limite) {
+    resultado = true;
+  }
+  return resultado;
+}
+
+void proteccion() {
+  if (limite_derecha() == true || limite_izquierda() == true){
+    if (estado == 1) {
+      neutro();
+    }
+  }
+}
 
 //Funciones de control de motores
 void neutro() {
@@ -102,6 +141,7 @@ void neutro() {
   analogWrite(izq2, 0);
   analogWrite(der, 0);
   analogWrite(der2, 0);
+  estado = 0;
 }
 
 void frenar(int potencia) {
@@ -109,6 +149,7 @@ void frenar(int potencia) {
   analogWrite(izq2, potencia);
   analogWrite(der, potencia);
   analogWrite(der2, potencia);
+  estado = 5;
 }
 
 
@@ -118,6 +159,7 @@ void avanzar(int potencia) {
   analogWrite(izq2, 0);
   analogWrite(der, potencia);
   analogWrite(der2, 0);
+  estado = 1;
 }
 
 void retroceder(int potencia) {
@@ -126,6 +168,7 @@ void retroceder(int potencia) {
   analogWrite(izq2, potencia);
   analogWrite(der, 0);
   analogWrite(der2, potencia);
+  estado = 2;
 }
 
 void izquierda(int potencia) {
@@ -134,6 +177,7 @@ void izquierda(int potencia) {
   analogWrite(izq2, potencia);
   analogWrite(der, potencia);
   analogWrite(der2, 0);
+  estado = 3;
 }
 
 void derecha(int potencia) {
@@ -142,14 +186,15 @@ void derecha(int potencia) {
   analogWrite(izq2, 0);
   analogWrite(der, 0);
   analogWrite(der2, potencia);
+  estado = 4;
 }
 
-
-void sacudir(int sensibilidad, int repeticiones) {
-  for (int i = 0; i < repeticiones; i++) {
-    izquierda(255);
-    delay(sensibilidad);
+void sacudir() {
+  if (estado == 3) {
     derecha(255);
-    delay(sensibilidad*2);
+  }else{
+    izquierda(255);
   }
 }
+
+
